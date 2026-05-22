4df3a30213547fd27be7dd9ba2e448b041b692e6
oydinoy <oydinoykamolova1@gmail.com>
2026-05-22 17:24:01 +0500

    N2 task teacher

---
+++ G2-N9/Program.cs
@@ -0,0 +1,75 @@
+﻿#region kopaytma
+//    int kopaytma(int x, int y)
+//{
+//    return x * y;
+//}
+
+//int natija1 = kopaytma(x: 9, y: 17);
+//Console.WriteLine(natija1);
+
+//int natija2 = kopaytma(x: 6, y: 18);
+//Console.WriteLine(natija2);
+#endregion
+#region ayirma
+//int ayrma(int x, int y)
+//{
+//    return x - y;
+//}
+
+//int natija1 = ayrma(x: 9, y: 17);
+//Console.WriteLine(natija1);
+
+//int natija2 = ayrma(x: 6, y: 18);
+//Console.WriteLine(natija2);
+#endregion
+#region bolinma
+//int bolinma(int x, int y)
+//{
+//    return x / y;
+//}
+
+//int natija1 = bolinma(x: 9, y: 17);
+//Console.WriteLine(natija1);
+
+//int natija2 = bolinma(x: 6, y: 18);
+//Console.WriteLine(natija2);
+#endregion
+
+#region masala 1
+//void oquvchilarnibaolash(string ism, int ball)
+//{
+//    if (ball >= 90)
+//    {
+//        Console.WriteLine($"Alo natija {ism}! siz {ball} ball oldiz");
+//    }
+//    else if (ball >= 70)
+//    {
+//        Console.WriteLine($"norm natija, {ism}! siz {ball} bal olding");
+//    }
+//    else if (ball >= 50)
+//    {
+//        Console.WriteLine($"qoloq, {ism}! siz {ball} bal olding");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"olibsan ol bu kuningdan qoloq miya,{ism}! siz{ball}ola olmading qoloq ");
+//    }
+
+//}
+//oquvchilarnibaolash("oydinoy", 100);
+//oquvchilarnibaolash("nozi", 80);
+//oquvchilarnibaolash("noila", 90);
+//oquvchilarnibaolash("anora", 100);
+
+
+#endregion
+
+
+#region masla 2
+//void Ismbnsalomlashish(string ism)
+//{
+//    Console.WriteLine($"ma gap jigar men oydinoyman uu 17 yowman opca dee, {ism}!");
+//}
+
+//Ismbnsalomlashish("Bobur");
+#endregion
---
+++ N8-HT1/Program.cs
@@ -0,0 +1,65 @@
+﻿#region 1 masala
+//string ism, familiya;
+//int yosh;
+
+//Console.Write("Ismingizni kiriting: ");
+//ism = Console.ReadLine().Trim();
+
+//Console.Write("Familiyangizni kiriting: ");
+//familiya = Console.ReadLine().Trim();
+
+//Console.Write("Yoshingizni kiriting: ");
+//yosh = int.Parse(Console.ReadLine().Trim());
+
+//Console.WriteLine($"Ismingiz: {ism}, Familiyangiz: {familiya}, Yoshingiz: {yosh}");
+#endregion
+#region 2 masala
+//string jumla;
+
+//Console.Write("Jumla kiriting: ");
+//jumla = Console.ReadLine().Trim();
+//string[] sozlar = jumla.Split(' ');
+//for (int i = 0; i < sozlar.Length; i++)
+//{
+//    char[] harflar = sozlar[i].ToCharArray();
+//    Array.Reverse(harflar);
+//    sozlar[i] = new string(harflar);
+//}
+//string natija = string.Join(" ", sozlar);
+
+//Console.WriteLine($"Natija: {natija}");
+#endregion 
+#region 3 masala
+//using System;
+//using System.Text;
+
+//StringBuilder jadval = new StringBuilder();
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    for (int j = 1; j <= 5; j++)
+//    {
+//        jadval.AppendLine($"{i} x {j} = {i * j}");
+//    }
+//}
+
+//Console.WriteLine(jadval.ToString());
+#endregion
+#region 4 masala
+using System;
+using System.Text;
+
+string text = "  C#   juda   zo'r   til.   Har   kun   mashq   qil!  ";
+
+string[] sozlar = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine($"So'zlar soni: {sozlar.Length}");
+
+StringBuilder natija = new StringBuilder();
+
+foreach (string soz in sozlar)
+{
+    natija.AppendLine(soz);
+}
+Console.WriteLine(natija.ToString());
+#endregion
